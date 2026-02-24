f176a9ec83d9ca8a0a9f7b1c8344ce7b66095961
ArseniyAbrosimov <senyaabrosimov@mail.ru>
2026-02-24 23:51:56 +0300

    Completed

--- user/test_add.c
+++ user/test_add.c
@@ -8,7 +8,6 @@ uint ERRARGSLEN = 20;
 uint ERRCHRLEN = 22;
 
 uint SPACEASCII = 32;
-uint MINUSASCII = 45;
 uint MINIMUMNUM = 48;
 uint MAXIMUMNUM = 57;
 
@@ -22,8 +21,6 @@ main(int argc, char *argv[])
 
   int num2_pos = 0;
   
-  int a_lth0 = 0, b_lth0 = 0;
-
   for(i=0; i+1 < MAX; ){
     cc = read(0, &c, 1);
     if(cc < 0) {
@@ -37,7 +34,6 @@ main(int argc, char *argv[])
       break;
 
     int is_space = (c == SPACEASCII ? 1 : 0);
-    int is_minus = (c == MINUSASCII ? 1 : 0);
     int is_num = (c >= MINIMUMNUM && c <= MAXIMUMNUM ? 1 : 0);
 
     if (is_space) {
@@ -47,15 +43,6 @@ main(int argc, char *argv[])
       } else {
         num2_pos = i + 1;
       }
-    } else if (is_minus) {
-      if (i != 0 && i != num2_pos) {
-        write (2, "Not a number (stdout)\n", ERRCHRLEN);
-        exit(1);
-      } else if (i == 0) {
-        a_lth0 = 1;
-      } else {
-        b_lth0 = 1;
-      }
     } else if (!is_num) {
       write (2, "Not a number (stdout)\n", ERRREADLEN);
       exit(1);     
@@ -64,15 +51,18 @@ main(int argc, char *argv[])
   }
 
   buf[i] = '\0';
-  if (i == num2_pos) {
+
+  if (i == num2_pos || num2_pos == 0) {
     write (2, "Wrong args (stdout)\n", ERRARGSLEN);
     exit(1);
   }
   
   printf("|%s|\n", buf);
 
-  int a = (a_lth0 ? -1 * atoi(buf + 1) : atoi(buf));
-  int b = (b_lth0 ? -1 * atoi(buf + num2_pos + 1) : atoi(buf + num2_pos));
+  buf[num2_pos - 1] = '\0';
+
+  int a = atoi(buf);
+  int b = atoi(buf + num2_pos);
   
   int sum;
   sum = add(a, b);
